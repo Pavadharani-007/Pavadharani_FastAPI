2605f80bf4f4c7a5a0ebf7f55fb3436914ee4f3d
Pavadharani-007 <144235209+Pavadharani-007@users.noreply.github.com>
2024-07-21 22:16:45 +0530

    Add files via upload

---
+++ schemas.py
@@ -0,0 +1,64 @@
+from typing import List,Optional
+from pydantic import BaseModel
+
+
+class EmployeeBase(BaseModel):
+    name: str
+    email: str
+    designation: str
+    payroll: str
+    company_id: int 
+
+class ProjectBase(BaseModel):
+    title: str
+    description: str
+
+class ProjectCreate(ProjectBase):
+    pass
+
+class EmployeeCreate(EmployeeBase):
+    pass
+
+class Employee(EmployeeBase):
+    id: int
+    
+    class Config:
+        orm_mode = True
+
+class Project(ProjectBase):
+    id: int
+    class Config:
+        orm_mode = True
+
+class EmployeeWithProjects(EmployeeBase):
+    id: int
+    projects: List[Project] = []
+    class Config:
+        orm_mode = True
+
+class ProjectWithEmployees(ProjectBase):
+    id: int
+    employees: List[Employee] = []
+    class Config:
+        orm_mode = True
+class CompanyWithEmployees(EmployeeBase):
+    id:int
+    employees:List[Employee] = []
+    class Config:
+        orm_mode = True
+
+class CompanyBase(BaseModel):
+    name: str
+    gst_no:str
+
+class CompanyCreate(CompanyBase):
+    pass
+
+class Company(CompanyBase):
+    id: int
+    
+
+    class Config:
+        orm_mode = True
+
+
